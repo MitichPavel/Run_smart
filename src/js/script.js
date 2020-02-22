4d98f60d4c90daf55e94326f5ece9d63fd9ad0d7
Pavlo <mitich.pavel@gmail.com>
2020-02-22 01:12:15 +0100

    finished RWD for catalog and slider

--- src/js/script.js
+++ src/js/script.js
@@ -75,8 +75,8 @@
 
 $(document).ready(function(){
 	$('.carousel__inner').slick({
+		autoplay: true,
 		speed: 1200,
-		// adaptiveHeight: true,
 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
 		responsive: [
